d053a3f454faf08f1d8ad46e6303fa5775f5126f
Aleksao998 <aleksaopacic988@gmail.com>
2023-03-13 23:50:24 +0100

    Create playground

---
+++ README.md
@@ -0,0 +1,47 @@
+<a name="readme-top"></a>
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <img src="https://user-images.githubusercontent.com/2770565/223762290-44afc792-8ad4-4dbb-b2c2-532780d6c5de.png" alt="Logo" width="80" height="80">
+  <h3 align="center">Lava SDK Playground</h3>
+  </p>
+</div>
+
+<b>Have fun, the Lava way 🌋</b>
+
+This is a simple node.js application that allows you to test the functionality of the lava-sdk library. The application is pre-configured with the latest version of the lava-sdk, and you can modify the sample code provided to test different requests against the Lava network.
+
+<!-- Prerequisites -->
+
+## Prerequisites
+
+To use this playground, you must have Node.js installed on your system. You can download Node.js from the official website.
+
+<!-- Prerequisites -->
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- Installation -->
+
+## Installation
+
+The best way to use this playground is to clone the repository and install dependencies:
+
+```sh
+git clone https://github.com/yourusername/lava-sdk-playground.git
+cd lava-sdk-playground
+npm install
+```
+
+Alternatively, you can also download the source code as a ZIP file and extract it to your desired directory.
+
+Make sure to have Node.js and npm installed on your machine before running the playground.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- USAGE EXAMPLES -->
+
+## Usage
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ src/playground.js
@@ -0,0 +1,47 @@
+const { LavaSDK } = require("lava-sdk");
+
+/**
+ * This is a playground for testing LavaSDK methods.
+ *
+ * To use this playground, simply add your private key, chainID and other required
+ * parameters while initializing the LavaSDK object. You can then use the `sendRelay`
+ * method to query the network using either the jsonrpc, tendermintrpc or rest interface.
+ *
+ * If you are using the rest interface, make sure to comment out lines 12-14 and uncomment
+ * the second script to send relay requests.
+ *
+ */
+async function main() {
+  const sdk = await new LavaSDK({
+    privateKey: "<private key from Cosmos Hub staked client>",
+    chainID: "chainID of the network you want to query",
+    jsonRPC: "jsonrpc method which will be used [tendermintrpc, jsonrpc, rest]", // optional
+    pairingListConfig:
+      "<pairing list config, if empty the public testent will be used>", // optional,
+    geolocation: "<geolocation of providers>", //optional
+  });
+
+  // Send relay request for a jsonrpc/tendermintrpc method
+  const response = await sdk.sendRelay({
+    method: "<method name>",
+    params: ["<array of params>"],
+  });
+
+  console.log(response);
+
+  // Important //
+  // If you are using rest rpcInterface for querying the network
+  // comment from the line 12-14 and uncomment seconds script for sending relays
+  /*
+    Send relay request for a rest method
+    const response = await sdk.sendRelay({
+      method: "<request method [GET/POST]>",
+      url: "URL of the request",
+      data: "query params"
+    });
+  */
+}
+
+(async () => {
+  await main();
+})();
